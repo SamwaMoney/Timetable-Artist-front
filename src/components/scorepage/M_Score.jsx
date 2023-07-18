af9ace439d32c69b87d4c153240a791f323bc75b
hanby-choi <hanby.choi@ewhain.net>
2023-07-18 16:07:42 +0900

    feat: 모바일 시간표 점수 페이지 no data 화면 추가 및 border, 햄버거 수정

--- src/components/scorepage/M_Score.jsx
+++ src/components/scorepage/M_Score.jsx
@@ -10,7 +10,8 @@ import twitter from '../../assets/scorepage/twitter.svg';
 import Hamburger from '../_common/Hamburger';
 import RangkingModal from '../_common/RankingModal';
 import EditModal from '../_common/EditModal';
-import MHamburgerButton from '../_common/M_HamburgerBtn';
+import M_ScoreHamburgerBtn from './M_ScoreHamburgerBtn';
+import BackBtn from '../_common/BackBtn';
 const M_Score = () => {
     const [data, setData] = useState([1]);
     const [isRankingModalOpen, setRankingModalOpen] = useState(false);
@@ -30,108 +31,123 @@ const M_Score = () => {
 
     return (
         <S.Wrapper>
-            <S.ScoreContainer>
-                <MHamburgerButton />
-                <S.Title>
-                    <div className='score'>내 시간표의 점수는...</div>
-                </S.Title>
-                <S.Score>76점!</S.Score>
-                <S.Img src={timetable} />
-                <S.Save>📁 시간표_이미지_저장하기</S.Save>
-            </S.ScoreContainer>
-            <S.TypeContainer>
-                <S.Title>
-                    <div className='type'>내 시간표 유형은?</div>
-                </S.Title>
-                <S.Score>
-                    <div className='type'>전공 마스터</div>
-                </S.Score>
-                <S.TypeImg src={type}></S.TypeImg>
-                <S.ResultContainer>
-                    <S.Part>
-                        <div className='good'>
-                            {'짱!\n\n\nദി(☆⸝⸝ᵔ‿ᵔ⸝⸝)\n\n'}
-                        </div>
-                    </S.Part>
-                    <S.ResultBox>
-                        <div className='text'>
-                            <span className='plus'>+ </span>
-                            최고 인기, 금공강!
-                        </div>
-                        <div className='text'>
-                            <span className='plus'>+ </span>1교시 제로
-                        </div>
-                        <div className='text'>
-                            <span className='plus'>+ </span>
-                            2연강까지만 용납
-                        </div>
-                    </S.ResultBox>
-                </S.ResultContainer>
-                <S.ResultContainer>
-                    <S.Part>
-                        <div className='bad'>
-                            {'음...\n\n\n(?・・);σ   \n\n'}
-                        </div>
-                    </S.Part>
-                    <S.ResultBox>
-                        <div className='text'>
-                            <span className='minus'>- </span>
-                            퐁당퐁당 수요일
-                        </div>
-                    </S.ResultBox>
-                </S.ResultContainer>
-                <S.Special>*。・✩━스페-셜━ ✩・*。</S.Special>
-                <S.SpecialBox>
-                    <div className='text'>✔ 아침잠 사수! 저녁형 시간표</div>
-                    <div className='text'>
-                        ✔ 채플? 그게 뭐죠. -.- 채플자유인
-                    </div>
-                    <div className='text'>✔ 6전공! 전공마스터</div>
-                </S.SpecialBox>
-                <S.Hide>
-                    {isHidden ? (
-                        <button
-                            onClick={() => {
-                                setIsHidden(false);
-                            }}
-                            style={{ background: 'none' }}
-                        >
-                            <S.Icon src={sharing_icon} width={'5vw'} />
-                        </button>
-                    ) : (
-                        <button
-                            onClick={() => {
-                                setIsHidden(true);
-                            }}
-                            style={{ background: 'none' }}
-                        >
-                            <S.Icon src={sharing_none} width={'5vw'} />
-                        </button>
-                    )}
-                    <div className='text'>강의명을 숨기고 게시할래요.</div>
-                </S.Hide>
-                {isUploaded ? (
-                    <S.UploadedBtn>
-                        이미 랭킹보드에 게시 완료되었어요
-                    </S.UploadedBtn>
-                ) : (
-                    <S.UploadBtn onClick={handleRankingClick}>
-                        랭킹보드에 게시하기
-                    </S.UploadBtn>
-                )}
+            {data.length !== 0 ? (
+                <>
+                    <S.ScoreContainer>
+                        <M_ScoreHamburgerBtn />
+                        <S.Title>
+                            <div className='score'>내 시간표의 점수는...</div>
+                        </S.Title>
+                        <S.Score>76점!</S.Score>
+                        <S.Img src={timetable} />
+                        <S.Save>📁 시간표_이미지_저장하기</S.Save>
+                    </S.ScoreContainer>
+                    <S.TypeContainer>
+                        <S.Title>
+                            <div className='type'>내 시간표 유형은?</div>
+                        </S.Title>
+                        <S.Score>
+                            <div className='type'>전공 마스터</div>
+                        </S.Score>
+                        <S.TypeImg src={type}></S.TypeImg>
+                        <S.ResultContainer>
+                            <S.Part>
+                                <div className='good'>
+                                    {'짱!\n\n\nദി(☆⸝⸝ᵔ‿ᵔ⸝⸝)\n\n'}
+                                </div>
+                            </S.Part>
+                            <S.ResultBox>
+                                <div className='text'>
+                                    <span className='plus'>+ </span>
+                                    최고 인기, 금공강!
+                                </div>
+                                <div className='text'>
+                                    <span className='plus'>+ </span>1교시 제로
+                                </div>
+                                <div className='text'>
+                                    <span className='plus'>+ </span>
+                                    2연강까지만 용납
+                                </div>
+                            </S.ResultBox>
+                        </S.ResultContainer>
+                        <S.ResultContainer>
+                            <S.Part>
+                                <div className='bad'>
+                                    {'음...\n\n\n(?・・);σ   \n\n'}
+                                </div>
+                            </S.Part>
+                            <S.ResultBox>
+                                <div className='text'>
+                                    <span className='minus'>- </span>
+                                    퐁당퐁당 수요일
+                                </div>
+                            </S.ResultBox>
+                        </S.ResultContainer>
+                        <S.Special>*。・✩━스페-셜━ ✩・*。</S.Special>
+                        <S.SpecialBox>
+                            <div className='text'>
+                                ✔ 아침잠 사수! 저녁형 시간표
+                            </div>
+                            <div className='text'>
+                                ✔ 채플? 그게 뭐죠. -.- 채플자유인
+                            </div>
+                            <div className='text'>✔ 6전공! 전공마스터</div>
+                        </S.SpecialBox>
+                        <S.Hide>
+                            {isHidden ? (
+                                <button
+                                    onClick={() => {
+                                        setIsHidden(false);
+                                    }}
+                                    style={{ background: 'none' }}
+                                >
+                                    <S.Icon src={sharing_icon} width={'5vw'} />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => {
+                                        setIsHidden(true);
+                                    }}
+                                    style={{ background: 'none' }}
+                                >
+                                    <S.Icon src={sharing_none} width={'5vw'} />
+                                </button>
+                            )}
+                            <div className='text'>
+                                강의명을 숨기고 게시할래요.
+                            </div>
+                        </S.Hide>
+                        {isUploaded ? (
+                            <S.UploadedBtn>
+                                이미 랭킹보드에 게시 완료되었어요
+                            </S.UploadedBtn>
+                        ) : (
+                            <S.UploadBtn onClick={handleRankingClick}>
+                                랭킹보드에 게시하기
+                            </S.UploadBtn>
+                        )}
 
-                <S.BasicFont>SNS에 공유하기</S.BasicFont>
-                <S.IconContainer>
-                    <S.Icon src={instagram} width={'12vw'} />
-                    <S.Icon src={kakaotalk} width={'12vw'} />
-                    <S.Icon src={twitter} width={'12vw'} />
-                </S.IconContainer>
-                {isUploaded && (
-                    <S.UploadBtn onClick={e => handleEditClick(e)}>
-                        시간표 수정하기
-                    </S.UploadBtn>
-                )}
-            </S.TypeContainer>
+                        <S.BasicFont>SNS에 공유하기</S.BasicFont>
+                        <S.IconContainer>
+                            <S.Icon src={instagram} width={'12vw'} />
+                            <S.Icon src={kakaotalk} width={'12vw'} />
+                            <S.Icon src={twitter} width={'12vw'} />
+                        </S.IconContainer>
+                        {isUploaded && (
+                            <S.UploadBtn onClick={e => handleEditClick(e)}>
+                                시간표 수정하기
+                            </S.UploadBtn>
+                        )}
+                    </S.TypeContainer>
+                </>
+            ) : (
+                <S.NoData>
+                    <BackBtn />
+                    <S.NoDataText>Σ(‘⊙ₒ ⊙’；)</S.NoDataText>
+                    <S.NoDataText>아직 시간표가 없어요!</S.NoDataText>
+                    <S.Button>시간표 만들러 가기</S.Button>
+                </S.NoData>
+            )}
         </S.Wrapper>
     );
 };
--- src/components/scorepage/M_ScoreHamburgerBtn.jsx
+++ src/components/scorepage/M_ScoreHamburgerBtn.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import hamburgerIcon from '../../assets/_common/hamburger.svg';
-const MHamburgerButton = () => {
+const M_ScoreHamburgerBtn = () => {
     const navigate = useNavigate();
 
     const onMoveMenuPage = () => {
@@ -14,19 +14,14 @@ const MHamburgerButton = () => {
     );
 };
 
-export default MHamburgerButton;
+export default M_ScoreHamburgerBtn;
 
 const Wrapper = styled.div`
-    z-index: 100;
     width: 100%;
-    height: 20vw;
     display: flex;
     align-items: center;
     padding-top: 6vh;
     padding-left: 5vw;
-    position: fixed;
-    top: 0;
-    background-color: var(--background);
 `;
 const HamburgerButton = styled.img`
     position: relative;
