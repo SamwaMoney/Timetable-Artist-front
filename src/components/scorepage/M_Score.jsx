933a4c90ae1a0490cd08eb54a3a3751bd5037e67
hanby-choi <hanby.choi@ewhain.net>
2023-07-29 22:58:26 +0900

    feat: 모바일 시간표 채점 결과 이미지 저장 추가

--- src/components/scorepage/M_Score.jsx
+++ src/components/scorepage/M_Score.jsx
@@ -1,5 +1,6 @@
 import { S } from './M_Score.style';
 import { useState } from 'react';
+import html2canvas from 'html2canvas';
 import timetable from '../../assets/scorepage/timetable.png';
 import type from '../../assets/scorepage/Rectangle 98.png';
 import sharing_icon from '../../assets/scorepage/sharing_option.svg';
@@ -7,11 +8,11 @@ import sharing_none from '../../assets/scorepage/sharing_none.svg';
 import instagram from '../../assets/scorepage/instagram.svg';
 import kakaotalk from '../../assets/scorepage/kakaotalk.svg';
 import twitter from '../../assets/scorepage/twitter.svg';
-import Hamburger from '../_common/Hamburger';
 import RangkingModal from '../_common/RankingModal';
 import EditModal from '../_common/EditModal';
 import M_ScoreHamburgerBtn from './M_ScoreHamburgerBtn';
 import BackBtn from '../_common/BackBtn';
+
 const M_Score = () => {
     const [data, setData] = useState([1]);
     const [isRankingModalOpen, setRankingModalOpen] = useState(false);
@@ -29,70 +30,95 @@ const M_Score = () => {
         setIsEditModalOpen(true);
     };
 
+    const onCapture = () => {
+        html2canvas(document.getElementById('capture')).then(canvas => {
+            let resultImg = canvas.toDataURL('image/png');
+            onSaveAs(resultImg, 'timetable-result.png');
+        });
+    };
+
+    const onSaveAs = (uri, fileName) => {
+        let link = document.createElement('a');
+        document.body.appendChild(link);
+        link.href = uri;
+        link.download = fileName;
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <S.Wrapper>
             {data.length !== 0 ? (
                 <>
-                    <S.ScoreContainer>
-                        <M_ScoreHamburgerBtn />
-                        <S.Title>
-                            <div className='score'>내 시간표의 점수는...</div>
-                        </S.Title>
-                        <S.Score>76점!</S.Score>
-                        <S.Img src={timetable} />
-                        <S.Save>📁 시간표_이미지_저장하기</S.Save>
-                    </S.ScoreContainer>
-                    <S.TypeContainer>
-                        <S.Title>
-                            <div className='type'>내 시간표 유형은?</div>
-                        </S.Title>
-                        <S.Score>
-                            <div className='type'>전공 마스터</div>
-                        </S.Score>
-                        <S.TypeImg src={type}></S.TypeImg>
-                        <S.ResultContainer>
-                            <S.Part>
-                                <div className='good'>
-                                    {'짱!\n\n\nദി(☆⸝⸝ᵔ‿ᵔ⸝⸝)\n\n'}
+                    <M_ScoreHamburgerBtn />
+                    <div id='capture'>
+                        <S.ScoreContainer>
+                            <S.Title>
+                                <div className='score'>
+                                    내 시간표의 점수는...
                                 </div>
-                            </S.Part>
-                            <S.ResultBox>
+                            </S.Title>
+                            <S.Score>76점!</S.Score>
+                            <S.Img src={timetable} />
+                            <S.Save onClick={onCapture}>
+                                📁 결과_이미지_저장하기
+                            </S.Save>
+                        </S.ScoreContainer>
+                        <S.TypeContainer>
+                            <S.Title>
+                                <div className='type'>내 시간표 유형은?</div>
+                            </S.Title>
+                            <S.Score>
+                                <div className='type'>전공 마스터</div>
+                            </S.Score>
+                            <S.TypeImg src={type}></S.TypeImg>
+                            <S.ResultContainer>
+                                <S.Part>
+                                    <div className='good'>
+                                        {'짱!\n\n\nദി(☆⸝⸝ᵔ‿ᵔ⸝⸝)\n\n'}
+                                    </div>
+                                </S.Part>
+                                <S.ResultBox>
+                                    <div className='text'>
+                                        <span className='plus'>+ </span>
+                                        최고 인기, 금공강!
+                                    </div>
+                                    <div className='text'>
+                                        <span className='plus'>+ </span>1교시
+                                        제로
+                                    </div>
+                                    <div className='text'>
+                                        <span className='plus'>+ </span>
+                                        2연강까지만 용납
+                                    </div>
+                                </S.ResultBox>
+                            </S.ResultContainer>
+                            <S.ResultContainer>
+                                <S.Part>
+                                    <div className='bad'>
+                                        {'음...\n\n\n(?・・);σ   \n\n'}
+                                    </div>
+                                </S.Part>
+                                <S.ResultBox>
+                                    <div className='text'>
+                                        <span className='minus'>- </span>
+                                        퐁당퐁당 수요일
+                                    </div>
+                                </S.ResultBox>
+                            </S.ResultContainer>
+                            <S.Special>*。・✩━스페-셜━ ✩・*。</S.Special>
+                            <S.SpecialBox>
                                 <div className='text'>
-                                    <span className='plus'>+ </span>
-                                    최고 인기, 금공강!
+                                    ✔ 아침잠 사수! 저녁형 시간표
                                 </div>
                                 <div className='text'>
-                                    <span className='plus'>+ </span>1교시 제로
+                                    ✔ 채플? 그게 뭐죠. -.- 채플자유인
                                 </div>
-                                <div className='text'>
-                                    <span className='plus'>+ </span>
-                                    2연강까지만 용납
-                                </div>
-                            </S.ResultBox>
-                        </S.ResultContainer>
-                        <S.ResultContainer>
-                            <S.Part>
-                                <div className='bad'>
-                                    {'음...\n\n\n(?・・);σ   \n\n'}
-                                </div>
-                            </S.Part>
-                            <S.ResultBox>
-                                <div className='text'>
-                                    <span className='minus'>- </span>
-                                    퐁당퐁당 수요일
-                                </div>
-                            </S.ResultBox>
-                        </S.ResultContainer>
-                        <S.Special>*。・✩━스페-셜━ ✩・*。</S.Special>
-                        <S.SpecialBox>
-                            <div className='text'>
-                                ✔ 아침잠 사수! 저녁형 시간표
-                            </div>
-                            <div className='text'>
-                                ✔ 채플? 그게 뭐죠. -.- 채플자유인
-                            </div>
-                            <div className='text'>✔ 6전공! 전공마스터</div>
-                        </S.SpecialBox>
+                                <div className='text'>✔ 6전공! 전공마스터</div>
+                            </S.SpecialBox>
+                        </S.TypeContainer>
+                    </div>
+                    <S.ShareContainer>
                         <S.Hide>
                             {isHidden ? (
                                 <button
@@ -138,7 +164,7 @@ const M_Score = () => {
                                 시간표 수정하기
                             </S.UploadBtn>
                         )}
-                    </S.TypeContainer>
+                    </S.ShareContainer>
                 </>
             ) : (
                 <S.NoData>
@@ -147,6 +173,12 @@ const M_Score = () => {
                     <S.NoDataText>아직 시간표가 없어요!</S.NoDataText>
                     <S.Button>시간표 만들러 가기</S.Button>
                 </S.NoData>
+            )}
+            {isRankingModalOpen && (
+                <RangkingModal setRankingModalOpen={setRankingModalOpen} />
+            )}
+            {isEditModalOpen && (
+                <EditModal setIsEditModalOpen={setIsEditModalOpen} />
             )}
         </S.Wrapper>
     );
