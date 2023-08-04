1f97e1135bdf00871e0559df99ef989d26eea410
ooherin <555ohr@naver.com>
2023-08-05 00:13:31 +0900

    fix: 랭킹 로딩 수정

--- src/components/rankingpage/RankDetail.jsx
+++ src/components/rankingpage/RankDetail.jsx
@@ -6,7 +6,7 @@ import CmtTag from './rightSection/CmtTag';
 import RightSectionSkeleton from '../../skeleton/RightSectionSkeleton';
 import { useState, useEffect } from 'react';
 import AltTableImg from '../../assets/_common/altTable.png';
-
+import { useLocation } from 'react-router-dom';
 const RankDetail = ({
     memberId,
     currentUserId,
@@ -19,6 +19,9 @@ const RankDetail = ({
 }) => {
     const [currentUser, setCurrentUser] = useState();
     const [commentNum, setCommentNum] = useState();
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const sort = params.get('sort');
 
     useEffect(() => {
         setCommentNum();
@@ -30,6 +33,11 @@ const RankDetail = ({
         fetchDetailData(currentUserId);
         setLoading(false);
     }, [currentUserId]);
+
+    // useEffect(() => {
+    //     console.log(loading);
+    //     console.log(rankLoading);
+    // }, [loading, rankLoading]);
 
     return loading || rankLoading ? (
         <RightSectionSkeleton />
